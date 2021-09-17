b5d869c32f3da2d8fa17bb63c4d205f22b55f760
Karen Baltazar <a01707750@itesm.mx>
2021-09-17 09:52:50 -0500

    Tron

---
+++ tron.py
@@ -0,0 +1,104 @@
+"""
+Tron, classic arcade game.
+
+Exercises
+
+3. Allow the tron player to go around the edge of the screen.
+4. How would you create a computer player?
+
+"""
+
+from turtle import *
+from freegames import square, vector
+from random import randint
+#from threading import Thread
+#from playsound import playsound
+from pygame import mixer
+
+#playsound('giorno.mp3')
+
+p1xy = vector(-100, 0)
+p1aim = vector(4, 0)
+p1body = set()
+
+p2xy = vector(100, 0)
+p2aim = vector(-4, 0)
+p2body = set()
+
+def inside(head):
+    "Return True if head inside screen."
+    return -200 < head.x < 200 and -200 < head.y < 200
+
+def draw():
+    "Advance players and draw game."
+    p1xy.move(p1aim)
+    p1head = p1xy.copy()
+    
+    change = randint(0, 9)
+    if change == 0:
+        p2aim.rotate(90)
+    elif change == 1:
+        p2aim.rotate(-90)
+
+    p2xy.move(p2aim)
+    p2head = p2xy.copy()
+
+# -------------------------------------------   
+    if not inside(p1head) or p1head in p2body:
+        print('Player violet wins!')
+        return
+
+    if not inside(p2head) or p2head in p1body:
+        print('Player gold wins!')
+        return
+    
+    if not inside(p1head) or p1head in p1body:
+        print('Player gold wins!')
+        return
+
+    if not inside(p2head) or p2head in p2body:
+        print('Player violet wins!')
+        return
+# -------------------------------------------   
+
+    p1body.add(p1head)
+    p2body.add(p2head)
+
+    square(p1xy.x, p1xy.y, 3, 'violet')
+    square(p2xy.x, p2xy.y, 3, 'gold')
+    update()
+    ontimer(draw, 50)
+    
+# -------------------------------------------    
+# Función para abrir archivo de música
+#def music_func():
+#    playsound('cancion.mp3')
+    
+
+# Definir función que llama audio
+#music = Thread(target=music_func)
+#music.daemon = True
+
+# mixer.init()
+# mixer.music.load("giorno.mp3")
+# mixer.music.play()
+
+#music = mixer.music.load('giorno.mp3')
+#mixer.music.play(loops=-1)
+
+# Iniciar musica
+#music.start()
+# -------------------------------------------  
+
+setup(420, 420, 370, 0)
+title("Tron")
+bgcolor("black")
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: p1aim.rotate(90), 'Up')
+onkey(lambda: p1aim.rotate(90), 'Left')
+onkey(lambda: p1aim.rotate(-90), 'Right')
+onkey(lambda: p1aim.rotate(-90), 'Down')
+draw()
+done()
